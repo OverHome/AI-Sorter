1765c295ce2eb22ce4b05abe5f08a9b40e73bfb4
ivan <ivan7855060@gmail.com>
2022-09-23 23:37:13 +0300

    Ai base markup

--- AI/model.py
+++ AI/model.py
@@ -2,3 +2,10 @@
 class Ai:
     def __init__(self):
         pass
+
+    def predict(self, vacancy, candidate):# предсказание кандидата для вакансии 0...1
+        return 0 
+
+    def predicts(self, vacancy, candidates):# предсказания кандидатов для вакансии [0...1, ...]
+        return [self.predict(vacancy, candidate)
+         for candidate in candidates] 
