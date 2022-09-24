21b0f77074a6dd216992a65d8d23b64ae7ead50a
aleksey-ruban <alekseyruban555@gmail.com>
2022-09-24 17:36:50 +0300

    add db and templates

--- Server/server.py
+++ Server/server.py
@@ -1,10 +1,34 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+import sqlite3
+
+DATABASE = "/tmp/sorter.db"
+DEBUG = True
+SECRET_KEY = "veq7rcm3136bcr4310vb6x32zfhvbwk"
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path, "sorter.db")))
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
+
+
+def connect_db():
+    connection = sqlite3.connect(app.config["DATABASE"])
+    connection.row_factory = sqlite3.Row
+    return connection
+
+# Creating new table
+def create_table_db():      
+    db = connect_db()
+    with app.open_resource("/sql/__.sql", mode="r") as file:
+        db.cursor().executescript(file.read())
+    db.commit()
+    db.close()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+
