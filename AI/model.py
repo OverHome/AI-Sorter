f5523190a00e5b181625a55d84195350df06508c
ivan <ivan7855060@gmail.com>
2022-09-24 23:09:07 +0300

    predict hotfix

--- AI/model.py
+++ AI/model.py
@@ -3,9 +3,9 @@ class Ai:
     def __init__(self):
         pass
 
-    def predict(self, vacancy, candidate):# предсказание кандидата для вакансии 0...1
+    def predict(self, data):# предсказание кандидата для вакансии 0...1
         return 0 
 
-    def predicts(self, vacancy, candidates):# предсказания кандидатов для вакансии [0...1, ...]
-        return [self.predict(vacancy, candidate)
-         for candidate in candidates] 
+    def predicts(self, datas):# предсказания кандидатов для вакансии [0...1, ...]
+        return [self.predict(data)
+         for data in datas] 
