0f083b47a3f99615bef6364ce1b338e1c12df691
ivan <ivan7855060@gmail.com>
2022-09-24 23:23:43 +0300

    models py

--- AI/model.py
+++ AI/model.py
@@ -1,10 +1,13 @@
+from tensorflow.keras.models import load_model
+tf_model = load_model('/ai_learn/neural_wb.h5')
+
 
 class Ai:
     def __init__(self):
         pass
 
     def predict(self, data):# предсказание кандидата для вакансии 0...1
-        return 0 
+        tf_model.predict(data)
 
     def predicts(self, datas):# предсказания кандидатов для вакансии [0...1, ...]
         return [self.predict(data)
